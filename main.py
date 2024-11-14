75f69bec1d501b169160a6095c3f4111312d6d9c
Gracie <gstirk@byu.edu>
2024-11-14 13:42:23 -0700

    i hope this works

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-import numpy as np
 import pandas as pd
 import zipfile
 import plotly.express as px
 import matplotlib.pyplot as plt
 import requests
-from io import BytesIO
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-#from my_plots import *
+from my_plots import *
 import streamlit as st
 
 @st.cache_data
